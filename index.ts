97895635e3dbf16a394d1ca05e73068ddf48c0ee
ninok <1396039+ninok@users.noreply.github.com>
2019-04-28 20:50:36 +0200

    Added animated sphere

--- index.ts
+++ index.ts
@@ -6,9 +6,12 @@
 
 import { GeoCoordinates } from "@here/harp-geoutils";
 import { View } from "./View";
+import * as THREE from "three";
+import { MapViewEventNames, RenderEvent } from "@here/harp-mapview";
 
+const canvas = document.getElementById("map") as HTMLCanvasElement; 
 const app = new View({
-    canvas: document.getElementById("map") as HTMLCanvasElement
+    canvas
 });
 
 const mapView = app.mapView;
@@ -21,6 +24,35 @@ window.addEventListener("resize", () => {
     mapView.resize(window.innerWidth, window.innerHeight);
 });
 
-// center the camera to New York
+const singapore = new GeoCoordinates(1.3029095, 103.8494058, 0);
+// center the camera to Singapore
 mapView.camera.position.set(0, 0, 1600);
-mapView.geoCenter = new GeoCoordinates(1.3029095, 103.8494058, 0);
+mapView.geoCenter = singapore.clone();
+
+const sphereGeoCenter = singapore.clone();
+const projection = mapView.projection;
+const sphere = new THREE.Mesh(
+    new THREE.SphereGeometry(10, 10, 10),
+    new THREE.MeshStandardMaterial({color: "#ff0000", depthTest: true}));
+sphere.renderOrder = 10000;
+
+mapView.addEventListener(MapViewEventNames.Render, (event:RenderEvent) => {
+    const ms = event.time;
+    sphereGeoCenter.longitude = singapore.longitude + Math.cos(ms / 1000) / 1000;
+    sphereGeoCenter.latitude = singapore.latitude + Math.sin(ms / 1000) / 1000;
+
+    projection.projectPoint(sphereGeoCenter, sphere.position);
+    sphere.position.sub(mapView.worldCenter);
+});
+
+mapView.scene.add(sphere);
+
+function animateSphere()
+{
+    requestAnimationFrame(()=> {
+        mapView.update();
+        animateSphere();
+    });
+}
+
+animateSphere();
